8c3e5b7e4b9138fd4607a40df5dfbb856d3f3ed6
leokang94 <acccdang8659@gmail.com>
2023-07-05 14:30:08 +0900

    imp: 불필요한 부분 제거

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,8 @@
-import { useRouter } from "../lib/Router";
-import { useEffect } from "react";
-
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { currentIndex, history } = useRouter();
-
-  useEffect(() => {
-    console.log(
-      `currentIndex: ${currentIndex}, history: ${JSON.stringify(history)}`,
-    );
-  }, [currentIndex, history]);
-
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       {children}
